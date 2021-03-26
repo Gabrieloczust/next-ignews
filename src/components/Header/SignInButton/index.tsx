cf36c636570efdcb8a595c6193a682226ae59526
Gabriel Fagundes Oczust <gabriel.oczust@meta.com.br>
2021-03-26 16:51:13 -0300

    # Auth - Github

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,20 +1,27 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isLogged = true;
+  const [session] = useSession();
 
-  return isLogged ? (
-    <button type="button" className={styles.button}>
-      <FaGithub color="#eba417" />
-      Sign in with Github
+  return session ? (
+    <button type="button" className={styles.button} onClick={() => signOut()}>
+      <FaGithub color="#04d361" />
+
+      {session.user.name}
+
+      <FiX color="#737380" className={styles.close} />
     </button>
   ) : (
-    <button type="button" className={styles.button}>
-      <FaGithub color="#04d361" />
-      Gabriel Oczust
-      <FiX color="#737380" className={styles.close} />
+    <button
+      type="button"
+      className={styles.button}
+      onClick={() => signIn("github")}
+    >
+      <FaGithub color="#eba417" />
+      Sign in with Github
     </button>
   );
 }
