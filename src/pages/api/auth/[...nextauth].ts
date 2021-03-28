a6538bf331abf548e8099f549409ef09cf29af64
Gabriel Fagundes Oczust <gabriel.oczust@meta.com.br>
2021-03-27 22:10:20 -0300

    # Database - Save user to Database when SignIn

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -9,4 +11,35 @@ export default NextAuth({
       scope: "read:user",
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      try {
+        await fauna.query(
+          // se nao existir um usuario com o mesmo email
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            // if return true
+            // cria o usuario
+            q.Create(q.Collection("users"), {
+              data: {
+                email: user.email,
+              },
+            }),
+            // if retur false (else)
+            // busca o usuario
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+
+        return true;
+      } catch (e) {
+        console.warn(e);
+        return false;
+      }
+    },
+  },
 });
